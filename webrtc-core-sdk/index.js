5d78132412fb89095d51a8a46a4bbb86e31ae529
Ashish Rastogi <ashish.rastogi@exotel.in>
2022-12-29 17:38:27 +0530

    core sdk bundling config

--- webrtc-core-sdk/index.js
+++ webrtc-core-sdk/index.js
@@ -1,1 +1,7 @@
-export * from  './src/webrtcSIPPhone';
+var pkg = require('./package.json');
+const version = pkg.version;
+const name = "webrtcsdk.js";
+export { name, version };
+
+
+export * from './src/webrtcSIPPhone';
