c90f2fb788923512e9e4dc069caa8245cd5f595f
Ashish Rastogi <ashish.rastogi@exotel.in>
2022-12-30 15:43:58 +0530

    refactor

--- webrtc-client-sdk/index.js
+++ webrtc-client-sdk/index.js
@@ -1,1 +1,5 @@
+var pkg = require('./package.json');
+const version = pkg.version;
+const name = "exotelsdk.js";
+
 export * from './src/listeners/ExWebClient';
--- webrtc-core-sdk/webpack.config.js
+++ webrtc-core-sdk/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename:'[name].js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
     library: 'webrtcSDK',
     globalObject: 'this',
