06570007e43ef7e90d8105f9260b6e13eee7ff7e
vijaykumar <vj.alternativ@gmail.com>
2023-07-20 16:54:09 +0530

    test

---
+++ webrtc-client-sdk/demo-non-npm/phone copy.js
@@ -0,0 +1,17 @@
+phone = '[\
+    {\
+        "Username":"vijayk0aa794fd",\
+        "DisplayName":"vijay",\
+        "HostServer":"voip.in1.exotel.com",\
+        "Domain":"siptrunkingpoc1m.voip.exotel.com",\
+        "Port":5071,\
+        "Password":"exotel321",\
+        "CallTimeout":1000,\
+        "Security": "wss",\
+        "EndPoint": "wss",\
+        "AccountSID":"exotelt1",\
+        "AccountNo":"ashishrb544b97d",\
+        "AutoRegistration": true\
+    }\
+]';
+
