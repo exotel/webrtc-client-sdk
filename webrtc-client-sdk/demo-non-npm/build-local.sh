0c2c7f9720e2b64b74127ec2e9e471fc21c62c0a
vijaykumar <vj.alternativ@gmail.com>
2023-07-20 23:29:53 +0530

    [VWS-14] fix race condition of sip register

---
+++ webrtc-client-sdk/demo-non-npm/build-local.sh
@@ -0,0 +1,3 @@
+#openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+
+rm -rf dist;cd ..;npm uninstall @exotel-npm-dev/webrtc-core-sdk;npm install ../webrtc-core-sdk;npm run build;cp -r dist demo-non-npm/;cd demo-non-npm;http-server  -S -C ~/pki/cert.pem -K ~/pki/key.pem
